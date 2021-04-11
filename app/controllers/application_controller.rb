5be47332cbb6ca3c03a7218fea687b4f61a09f88
test <test@example.com>
2021-04-11 03:30:53 +0000

    Exercise chapter 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "Hello, world"
+    render html: "¡Hola, mundo!"
+  end
+  def goodbye
+    render html: "goodbye, world!"
   end
 end
